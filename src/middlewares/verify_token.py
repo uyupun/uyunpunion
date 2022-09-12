9c86f4e23254bd34ada735bf8909da4d3fb17bad
Kazuki Tobita <kazukiti201@gmail.com>
2022-09-12 21:29:33 +0900

    Fix middleware bug

--- src/middlewares/verify_token.py
+++ src/middlewares/verify_token.py
@@ -5,12 +5,15 @@ from fastapi import Request, Response
 from fastapi.responses import JSONResponse
 from settings import get_settings
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.status import HTTP_401_UNAUTHORIZED
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED
 
 
 class VerifyTokenMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: Callable) -> Response:
         settings = get_settings()
+
+        if "UYUNPUNION-TOKEN" not in request.headers:
+            return JSONResponse(content={}, status_code=HTTP_400_BAD_REQUEST)
 
         if not secrets.compare_digest(
             settings.UYUNPUNION_TOKEN, request.headers["UYUNPUNION-TOKEN"]
