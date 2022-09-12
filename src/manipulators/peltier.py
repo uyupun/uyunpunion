61f811eccf185a4511ec1e36cb8e51e9d21aafdc
Kazuki Tobita <kazukiti201@gmail.com>
2022-09-13 00:10:25 +0900

    Modify manipulator

--- src/manipulators/peltier.py
+++ src/manipulators/peltier.py
@@ -1,9 +1,30 @@
+from enum import Enum
+
 from manipulators.manipulator import Manipulator
 
 
+class PeltierMode(Enum):
+    Cold: str = "cold"
+    Warm: str = "warm"
+
+
+class PeltierModeNotExistsError(Exception):
+    pass
+
+
 class PeltierManipulator(Manipulator):
-    def start(self):
+    def start(self, mode=PeltierMode.Cold):
+        if mode is PeltierMode.Cold:
+            return self._cold()
+        if mode is PeltierMode.Warm:
+            return self._warm()
+        raise PeltierModeNotExistsError
+
+    def stop(self):
         pass
 
-    def stop():
+    def _cold(self):
+        pass
+
+    def _warm(self):
         pass
