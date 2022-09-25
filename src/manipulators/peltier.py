b5d5b4fb6f0d0c041e46e2e923e2a86b159178bd
Kazuki Tobita <kazukiti201@gmail.com>
2022-09-25 11:40:16 +0900

    Add peltier manipulator

--- src/manipulators/peltier.py
+++ src/manipulators/peltier.py
@@ -1,5 +1,8 @@
 import sys
+import time
 from enum import Enum
+
+import pigpio
 
 try:
     from manipulators.manipulator import Manipulator
@@ -17,7 +20,23 @@ class PeltierModeNotExistsError(Exception):
 
 
 class PeltierManipulator(Manipulator):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.apwm = 19
+        self.ain1 = 20
+        self.ain2 = 21
+
+        self.pi = pigpio.pi()
+
+    def init_peltier(self):
+        for pin in [self.apwm, self.ain1, self.ain2]:
+            self.pi.set_mode(pin, pigpio.OUTPUT)
+            self.pi.write(pin, 0)
+
     def start(self, mode=PeltierMode.Cold):
+        self.init_peltier()
+
         if mode == PeltierMode.Cold:
             return self._cold()
         if mode == PeltierMode.Warm:
@@ -25,12 +44,24 @@ class PeltierManipulator(Manipulator):
         raise PeltierModeNotExistsError
 
     def stop(self):
+        self.init_peltier()
+
         print("peltier stop")
 
     def _cold(self):
+        self.pi.hardware_PWM(self.apwm, 1000, (100 * 10000))
+        self.pi.write(self.ain1, 1)
+        self.pi.write(self.ain2, 0)
+        time.sleep(0.5)
+
         print("peltier cold")
 
     def _warm(self):
+        self.pi.hardware_PWM(self.apwm, 1000, (100 * 10000))
+        self.pi.write(self.ain1, 0)
+        self.pi.write(self.ain2, 1)
+        time.sleep(0.5)
+
         print("peltier warm")
 
 
--- src/routes/peltier.py
+++ src/routes/peltier.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from manipulators.peltier import PeltierManipulator
+from manipulators.peltier import PeltierManipulator, PeltierMode
 from middlewares.verify_token import verify_token_middleware
 from shemas.peltier import ColdPeltierResponse, StopPeltierResponse, WarmPeltierResponse
 from shemas.token import TokenRequestHeader
@@ -17,6 +17,7 @@ def cold_peltier(
     """
     ペルチェ素子を冷却する
     """
+    manipulator.start(mode=PeltierMode.Cold)
     return {"message": "pong"}
 
 
@@ -28,6 +29,7 @@ def warm_peltier(
     """
     ペルチェ素子を加熱する
     """
+    manipulator.start(mode=PeltierMode.Warm)
     return {"message": "pong"}
 
 
@@ -39,4 +41,5 @@ def stop_peltier(
     """
     ペルチェ素子を停止する
     """
+    manipulator.stop()
     return {"message": "pong"}
