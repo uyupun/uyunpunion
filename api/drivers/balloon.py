37426560ee05f36bd229abff4447d2110d3bf285
NNN-kakimoto <sh_kakimoto@fancs.com>
2023-09-24 11:40:04 +0900

    Add balloon needle

--- api/drivers/balloon.py
+++ api/drivers/balloon.py
@@ -1,9 +1,28 @@
+import time
 from typing import Literal
+
+import pigpio
 
 
 class BalloonDriver:
+    def __init__(self) -> None:
+        super().__init__()
+        self.pi = pigpio.pi()
+        self.pin = 24
+
     def status(self):
         print("balloon status")
 
-    def needle(self, balloon_id: Literal[1, 2]):
+    def needle(self, balloon_id: Literal[1, 2]) -> None:
+        self.pi.set_servo_pulsewidth(self.pin, 1500)
+        time.sleep(1)
+
+        self.pi.set_servo_pulsewidth(self.pin, 2000)
+        time.sleep(1)
+
+        self.pi.set_servo_pulsewidth(self.pin, 1500)
+        time.sleep(1)
+
+        self.pi.set_servo_pulsewidth(self.pin, 0)
+        self.pi.stop()
         print("balloon needle")
